c21cbe2865bbc5e0a5d4edc0b2b39850f4fb6957
GalD101 <galdali10@gmail.com>
2024-10-13 11:47:05 +0300

    devcontainer

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Start Jupyter Notebook with a password
-jupyter notebook password <<EOF
-${{ secrets.PASS_WORD_DEVCONTAINER }}
-EOF
-
-# Start Jupyter Notebook in the background
-jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root &
+# Start Jupyter Notebook without a password or token
+jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password='' &
